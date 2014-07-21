ae660761e8d9ff3ca50973ada8ddff4cd6b92a9c
robotwholearned <robotwholearned88@gmail.com>
2014-07-21 00:50:22 -0600

    _s-bootstrap theme

---
+++ wp-content/themes/_s-bootstrap/functions.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * _s-bootstrap functions and definitions
+ *
+ * @package _s-bootstrap
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 640; /* pixels */
+}
+
+if ( ! function_exists( '_s_bootstrap_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function _s_bootstrap_setup() {
+
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on _s-bootstrap, use a find and replace
+	 * to change '_s-bootstrap-bootstrap' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( '_s-bootstrap-bootstrap', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	//add_theme_support( 'post-thumbnails' );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu', '_s-bootstrap-bootstrap' ),
+	) );
+	
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 * See http://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'video', 'quote', 'link'
+	) );
+
+	// Setup the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( '_s_bootstrap_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+}
+endif; // _s_bootstrap_setup
+add_action( 'after_setup_theme', '_s_bootstrap_setup' );
+
+/**
+ * Register widget area.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_sidebar
+ */
+function _s_bootstrap_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Sidebar', '_s-bootstrap-bootstrap' ),
+		'id'            => 'sidebar-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+}
+add_action( 'widgets_init', '_s_bootstrap_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function _s_bootstrap_scripts() {
+	wp_enqueue_style( '_s-bootstrap-bootstrap-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( '_s-bootstrap-bootstrap-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
+	wp_enqueue_script( '_s-bootstrap-bootstrap-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '3.0.1', true );
+
+    wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css', array(), '3.0.1', 'all' );
+
+    wp_enqueue_script( 'bootstrap-js' );
+
+    wp_enqueue_style( 'bootstrap-css' );
+
+    wp_register_style( 'custom-styles-css', get_template_directory_uri() . '/custom-styles.css', array(), '0.0.1','all' );
+
+	wp_enqueue_style( 'custom-styles-css');
+
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', '_s_bootstrap_scripts' );
+
+remove_filter ('the_content', 'wpautop');
+
+/**
+ * Implement the Custom Header feature.
+ */
+//require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/jetpack.php';
