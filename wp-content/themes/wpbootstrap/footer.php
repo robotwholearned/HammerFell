cf47de0007d3e17ed64d96b9a3b98a87755b79e2
robotwholearned <robotwholearned88@gmail.com>
2014-07-23 13:20:15 -0600

    bootstrap

--- wp-content/themes/wpbootstrap/footer.php
+++ wp-content/themes/wpbootstrap/footer.php
@@ -9,7 +9,14 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
   </body>
 </html>
+
+
+
+wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '3.0.1', true );
+wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css', array(), '3.0.1', 'all' );
+wp_enqueue_script( 'bootstrap-js' );
+wp_enqueue_style( 'bootstrap-css' );
